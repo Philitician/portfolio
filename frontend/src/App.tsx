9d89b41ff0af5f00ef3883a88257d048b68bc91c
Philip Rothschild <philrr16@uia.no>
2021-02-20 19:32:12 +0100

    Init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="text-pink-500">Snowpack with React & Tailwind CSS</div>
+    </div>
+  );
+}
+
+export default App;
